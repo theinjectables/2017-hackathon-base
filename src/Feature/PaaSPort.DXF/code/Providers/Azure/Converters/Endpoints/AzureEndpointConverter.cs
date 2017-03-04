73c74059c78bc5e873566a9e730953c59c88c607
Zachary Kniebel <zkniebel@delphicdigital.com>
2017-03-04 14:02:51 -0500

    Implemented Azure endpoint as part of Azure Provider for DXF, added TDS project for DXF with all relevant items for Azure provider and Azure Endpoint

---
+++ src/Feature/PaaSPort.DXF/code/Providers/Azure/Converters/Endpoints/AzureEndpointConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.DataExchange.Converters.Endpoints;
+using Sitecore.DataExchange.Models;
+using Sitecore.DataExchange.Repositories;
+using Sitecore.Services.Core.Model;
+using TheInjectables.Feature.PaaSPort.DXF.Providers.Azure.Models.ItemModels.Endpoints;
+using TheInjectables.Feature.PaaSPort.DXF.Providers.Azure.Plugins;
+
+namespace TheInjectables.Feature.PaaSPort.DXF.Providers.Azure.Converters.Endpoints
+{
+    public class AzureEndpointConverter : BaseEndpointConverter<ItemModel>
+    {
+        private static readonly Guid TemplateId = Guid.Parse("{9E41746D-B14C-4FFA-B70B-8F522901CC9E}");
+        public AzureEndpointConverter(IItemModelRepository repository) : base(repository)
+        {
+            //        //
+            //        //identify the template an item must be based
+            //        //on in order for the converter to be able to
+            //        //convert the item
+            SupportedTemplateIds.Add(TemplateId);
+        }
+
+        protected override void AddPlugins(ItemModel source, Endpoint endpoint)
+        {
+            //create the plugin
+            var settings = new AzureSettings()
+            {
+                TenantId = GetStringValue(source, AzureEndpointItemModel.TenantId),
+                Key = GetStringValue(source, AzureEndpointItemModel.Key),
+                Client = GetStringValue(source, AzureEndpointItemModel.Client),
+                Subscription = GetStringValue(source, AzureEndpointItemModel.Subscription),
+                ServiceManagerConfigurationPath = GetStringValue(source, AzureEndpointItemModel.ServiceManagerConfigurationPath)
+            };
+
+            //add the plugin to the endpoint
+            endpoint.Plugins.Add(settings);
+        }
+    }
+}
---
+++ src/Feature/PaaSPort.DXF/code/Providers/Azure/Plugins/AzureSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheInjectables.Foundation.PaaSPort.Abstractions.Client;
+
+namespace TheInjectables.Feature.PaaSPort.DXF.Providers.Azure.Plugins
+{
+    public class AzureSettings : Sitecore.DataExchange.IPlugin
+    {
+        public string TenantId { get; set; }
+        public string Key { get; set; }
+        public string Client { get; set; }
+        public string Subscription { get; set; }
+
+        public string ServiceManagerConfigurationPath { get; set; }
+        private IAzureManager _serviceManager;
+        public IAzureManager ServiceManager 
+            => _serviceManager ??
+                (_serviceManager = !string.IsNullOrEmpty(ServiceManagerConfigurationPath)
+                    ? Sitecore.Configuration.Factory.CreateObject(ServiceManagerConfigurationPath, true) as IAzureManager
+                    : null);
+
+        public AzureSettings()
+        {
+        }
+    }
+}
