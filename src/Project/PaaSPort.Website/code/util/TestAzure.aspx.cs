339fdff9cdb116e7e80f030308d6aa1d2a76abe5
pnavarra <pete@sitecorehacker.com>
2017-03-04 03:21:38 -0500

    Adding in publish profiles

---
+++ src/Project/PaaSPort.Website/code/util/TestAzure.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Sitecore.sitecore.admin;
+
+namespace TheInjectables.Project.PaaSPort.Website.util
+{
+    public partial class TestAzure : AdminPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CheckSecurity();
+
+            
+
+        }
+    }
+}
